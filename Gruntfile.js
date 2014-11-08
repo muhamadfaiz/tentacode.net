560efeb8bb69991a8e3eb592078b4bece0d287f8
tentacode <contact@gabrielpillet.com>
2014-11-08 12:13:02 +0100

    Better Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,63 +13,22 @@ module.exports = function(grunt) {
     dirs: {
       'vendor': '<%= bowerrc.directory %>',
       'jquery': {
-        'js': '<%= dirs.vendor %>/jquery',
+        'js': '<%= dirs.vendor %>/jquery/dist',
       },
       'bootstrap': {
-        'js': '<%= dirs.vendor %>/bootstrap/js',
-        'less': '<%= dirs.vendor %>/bootstrap/less'
+        'js': '<%= dirs.vendor %>/bootstrap/dist/js',
+        'less': '<%= dirs.vendor %>/bootstrap/less/bootstrap.less'
       },
       'blog': {
         'js': 'js',
         'less': 'less'
+      },
+      'built': {
+        'js': 'web/built/js',
+        'css': 'web/built/css'
       }
     },
 
-    src: {
-      output: {
-        'jquery': {
-          'js': 'web/built/jquery/js/jquery.js'
-        },
-        'bootstrap': {
-          'js': 'web/built/bootstrap/js/bootstrap.js',
-          'css': 'web/built/bootstrap/css/bootstrap.css'
-        },
-        'blog': {
-          'js': 'web/built/js/blog.js',
-          'css': 'web/built/css/blog.css'
-        }
-      },
-
-      input: {
-        jquery: {
-          'js': [
-            '<%= dirs.jquery.js %>/jquery.min.js'
-          ]
-        },
-        bootstrap: {
-          'js': [
-            '<%= dirs.bootstrap.js %>/dropdown.js',
-            '<%= dirs.bootstrap.js %>/tooltip.js',
-            '<%= dirs.bootstrap.js %>/collapse.js',
-            '<%= dirs.bootstrap.js %>/transition.js',
-            '<%= dirs.bootstrap.js %>/carousel.js'
-          ],
-          'less': [
-            '<%= dirs.bootstrap.less %>/bootstrap.less'
-          ]
-        },
-        blog: {
-          'js': [
-            '<%= dirs.blog.js %>/index.js'
-          ],
-          'less': [
-            '<%= dirs.blog.less %>/index.less'
-          ]
-        }
-      },
-    },
-
-    /********************************** Task **********************************/
     // Bower
     bower: {
       install: {
@@ -86,47 +45,57 @@ module.exports = function(grunt) {
 
     // Concatenation
     concat: {
-      blog: {
-        src: '<%= src.input.blog.js %>',
-        dest: '<%= src.output.blog.js %>'
+      js: {
+        src: [
+          '<%= dirs.jquery.js %>/jquery.min.js',
+          '<%= dirs.bootstrap.js %>/bootstrap.min.js',
+          '<%= dirs.blog.js %>/*.js',
+        ],
+        dest: '<%= dirs.built.js %>/blog.js'
       },
-      jquery: {
-        src: '<%= src.input.jquery.js %>',
-        dest: '<%= src.output.jquery.js %>'
-      },
-      bootstrap: {
-        src: '<%= src.input.bootstrap.js %>',
-        dest: '<%= src.output.bootstrap.js %>'
-      }
     },
 
     // LESS
     less: {
-      bootstrap: {
-        files: { '<%= src.output.bootstrap.css %>': '<%= src.input.bootstrap.less %>'}
+      options: {
+        compress: true,
+        cleancss: true,
       },
       blog: {
-        files: { '<%= src.output.blog.css %>': '<%= src.input.blog.less %>'}
+        files: { '<%= dirs.built.css %>/blog.css': [
+          '<%= dirs.bootstrap.less %>',
+          '<%= dirs.blog.less %>/*.less'
+        ]}
+      }
+    },
+
+    // Uglify
+    uglify: {
+      blog: {
+        files: {
+          '<%= dirs.built.js %>/blog.js': ['<%= dirs.built.js %>/blog.js']
+        }
       }
     },
 
     // Watch
     watch: {
-      blogless: {
+      less: {
         files: ['<%= dirs.blog.less %>/*.less'],
-        tasks: ['less:blog'],
+        tasks: ['less'],
         options: {
             spawn: false,
         },
       },
-      blogjs: {
+      js: {
         files: ['<%= dirs.blog.js %>/*.js'],
-        tasks: ['concat:blog'],
+        tasks: ['concat'],
         options: {
             spawn: false,
         },
       },
     },
+    
   });
 
   // Load plugins
@@ -134,14 +103,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Callable tasks
   grunt.registerTask('default', [
     'bower', 
-    'concat:jquery', 
-    'less:bootstrap', 
-    'concat:bootstrap', 
-    'less:blog', 
-    'concat:blog'
+    'concat',
+    'uglify',
+    'less',
   ]);
 };
